ffc8e5a990dbda7d8de0351a37a929b1a67123be
Pieng12 <jonathanmanikraja@gmail.com>
2024-12-18 13:40:08 +0700

    perbaikan tampilan

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,8 +43,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'username',
         'email',
         'password',
-        'role',
-        'google_id'
+        'role'
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->uuid('user_id')->primary();
             $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->string('google_id')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role', ['owner', 'cashier', 'user']);
